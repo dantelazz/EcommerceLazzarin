7b2a16e6b6b758981f9713cd51a9f828ff224c18
dantelazz <69008060+dantelazz@users.noreply.github.com>
2022-10-22 12:18:17 -0300

    Iiniciando proyecto

---
+++ src/Componentes/CartWidget/CartWidjet.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+
+
+function CartWidjet() {
+  return (
+    <div>
+      <img src='' ></img>
+    </div>
+  )
+}
---
+++ src/Componentes/ItemListContainer/ItemListContainer.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function ItemListContainer(prop) {
+  return (
+    <div>ItemListContainer</div>
+  )
+}
